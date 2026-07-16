e5d11d8d903e06a1e15316965d8299bf700974d7
Andrew Bell <andrew.bell.ia@gmail.com>
2026-07-16 13:58:50 -0400

    Hang.

---
+++ autotest/cpp/hang.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *
+ * Project:  GDAL Core
+ * Purpose:  Test fix for https://github.com/OSGeo/gdal/issues/1488 (concurrency
+ *issue with overviews) Author:   Even Rouault, <even dot rouault at spatialys
+ *dot com>
+ *
+ ******************************************************************************
+ * Copyright (c) 2019, Even Rouault <even dot rouault at spatialys dot com>
+ * Copyright (c) 2019, Thomas Bonfort <thomas.bonfort at gmail.com>
+ *
+ * SPDX-License-Identifier: MIT
+ ****************************************************************************/
+
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "gdal.h"
+#include "cpl_vsi_virtual.h"
+
+#include "gtest_include.h"
+
+namespace
+{
+
+TEST(hang, test)
+{
+    GDALAllRegister();
+
+    std::string url("/vsicurl/https://github.com/PDAL/data/raw/refs/heads/main/"
+                    "autzen/autzen-classified.copc.laz");
+    uint32_t size = 54;
+
+    std::thread t(
+        [url, size]
+        {
+            std::vector<char> buf(size);
+
+            VSILFILE *file = VSIFOpenL(url.c_str(), "rb");
+            VSIFSeekL(file, 375, SEEK_SET);
+            VSIFReadL(buf.data(), 1, size, file);
+            VSIFCloseL(file);
+            int sum = 0;
+            for (char c : buf)
+                sum += (uint8_t)c;
+            std::cerr << "Sum = " << sum << "!\n";
+        });
+
+    std::cerr << "Thread running!\n";
+    t.join();
+    std::cerr << "Thread joined!\n";
+}
+
+}  // namespace
